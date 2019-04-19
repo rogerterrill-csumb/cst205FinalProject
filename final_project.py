038ad4160a6c11fc0233611327950fb88e77e650
Roger Terrill <rchicasterill@gmail.com>
2019-04-19 15:02:13 -0700

    mockup navigation working

--- final_project.py
+++ final_project.py
@@ -20,7 +20,7 @@ class Room(object):
             print "That is not a valid direction"
 
     def print_room(self):
-        print self.x, self.y
+        # print self.x, self.y
         return self.x, self.y
 
     def reset(self):
@@ -33,7 +33,7 @@ class StartRoom(Room):
         super(StartRoom, self).__init__(x, y, *args)
 
     def print_room_text(self):
-        print "This is the intro"
+        print "***This is the intro***\n"
 
 
 class Hallway(Room):
@@ -41,7 +41,7 @@ class Hallway(Room):
         super(Hallway, self).__init__(x, y, *args)
 
     def print_room_text(self):
-        print "This is just a hallway, nothing here"
+        print "***This is just a hallway, nothing here***\n"
 
 
 class Cave(Room):
@@ -49,7 +49,23 @@ class Cave(Room):
         super(Cave, self).__init__(x, y, *args)
 
     def print_room_text(self):
-        print "You are in a cave"
+        print "***You are in a cave***\n"
+
+
+class OgreRoom(Room):
+    def __init__(self, x, y, *args):
+        super(OgreRoom, self).__init__(x, y, *args)
+
+    def print_room_text(self):
+        print "***You are in the ogre room***\n"
+
+
+class SecretRoom(Room):
+    def __init__(self, x, y, *args):
+        super(SecretRoom, self).__init__(x, y, *args)
+
+    def print_room_text(self):
+        print "***You are in the secret room***\n"
 
 
 def room_execute(room):
@@ -61,8 +77,11 @@ def room_execute(room):
 
 def main():
     start_room = StartRoom(0, 0, 'n', 's', 'e', 'w')
-    north_tile = Hallway(0, 1, 'n', 's')
-    cave_tile = Cave(0, -1, 'n', 's')
+    north_tile = Hallway(0, 1, 's')
+    cave_tile = Cave(0, -1, 'n')
+    ogre_tile = OgreRoom(0, -1, 'e')
+    secret_tile = SecretRoom(1, 0, 'w')
+
     location = start_room
     location.print_room_text()
 
@@ -71,17 +90,14 @@ def main():
         location.move(direction)
         if location.print_room() == (0, 0):
             location = room_execute(start_room)
-            # location = start_room
-            # location.reset()
-            # location.print_room_text()
         elif location.print_room() == (0, 1):
-            location = north_tile
-            location.reset()
-            location.print_room_text()
+            location = room_execute(north_tile)
         elif location.print_room() == (0, -1):
-            location = cave_tile
-            location.reset()
-            location.print_room_text()
+            location = room_execute(cave_tile)
+        elif location.print_room() == (-1, 0):
+            location = room_execute(ogre_tile)
+        elif location.print_room() == (1, 0):
+            location = room_execute(secret_tile)
 
         location.print_room()
 
