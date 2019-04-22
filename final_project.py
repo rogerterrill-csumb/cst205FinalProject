4bd1bf662490af330a4a94607c85d8494e8cfa06
RogerDodger <lpaddikt@gmail.com>
2019-04-21 22:39:58 -0700

    Working game with all conditions met, possibly add to not count directions that are not part of the map

--- final_project.py
+++ final_project.py
@@ -1,82 +1,90 @@
-# def sound(sound_file):
-#     sound = makeSound(sound_file)
-#     targetIndex = 0
-#     clip = makeEmptySound(15340)
-#     for ii in range(0, 15340):
-#         x = getSampleValueAt(sound, ii)
-#         setSampleValueAt(clip, targetIndex, x)
-#         targetIndex = targetIndex + 1
-#     max = maxVolume(clip)
-#     play(clip)
-#
-#
-# def maxSample(sound):
-#     max_val = 0
-#     for sample in getSamples(sound):
-#         max_val = max(max_val, getSampleValue(sample))
-#     return max_val
-#
-#
-# def maxVolume(sound):
-#     largest = maxSample(sound)
-#     maxPossibleSampleValue = 32767.0
-#     factor = float(maxPossibleSampleValue) / largest
-#     for sample in getSamples(sound):
-#         value = getSampleValue(sample)
-#         setSampleValue(sample, value * factor)
-#     return sample
-#
-#
-# def resize(pic):
-#     width = getWidth(pic)
-#     height = getHeight(pic)
-#     target = makeEmptyPicture(1000, 1000)
-#     percentWidth = width / 1000.0
-#     percentHeight = height / 1000.0
-#     for x in range(0, 999):
-#         for y in range(0, 999):
-#             color = getColor(getPixel(pic, int(x * percentWidth), int(y * percentHeight)))
-#             setColor(getPixel(target, x, y), color)
-#     return target
-#
-#
-# def win_image(image_file):
-#     pic = makePicture(image_file)
-#     pic = resize(pic)
-#     pixels = getPixels(pic)
-#     for p in pixels:
-#         newColor = makeColor(getRed(p) + 50, getGreen(p) + 50, getBlue(p) + 50)
-#         setColor(p, newColor)
-#
-#     phrase = "You win!"
-#     import java.awt.Font as Font
-#     myFont = makeStyle("Comic Sans", Font.BOLD, 80)
-#     addTextWithStyle(pic, 350, 500, phrase, myFont, black)
-#     repaint(pic)
-#
-#
-# def lose_image(image_file):
-#     pic = makePicture(image_file)
-#     pic = resize(pic)
-#     pixels = getPixels(pic)
-#     for p in pixels:
-#         avg_color = avg_color = (getRed(p) * .299 + getGreen(p) * .587 + getBlue(p) * .114)
-#         newColor = makeColor(avg_color, avg_color, avg_color)
-#         setColor(p, newColor)
-#
-#     phrase = "You lose."
-#     import java.awt.Font as Font
-#     myFont = makeStyle("Comic Sans", Font.BOLD, 80)
-#     addTextWithStyle(pic, 350, 500, phrase, myFont, red)
-#     repaint(pic)
-#
-#
-# def win():
-#     setMediaPath()
-#     win_image = getMediaPath() + 'win.jpg'
-#     lose_image = getMediaPath() + 'lose.jpg'
-#     win_sound = getMediaPath() + 'win.wav'
-#     lose_sound = getMediaPath() + 'lose.wav'
+__author__ = "Roger Terrill, Abby Packham, Carlos Orduna"
+__copyright__ = "Copyright 2019, CST205"
+__license__ = "GPL"
+__version__ = "1.0.1"
+__email__ = "rchicasterrill@csumb.edu, apackham@csumb.edu, cordunacorrales@csumb.edu  "
+__status__ = "Production"
+
+
+def sound(sound_file):
+    sound = makeSound(sound_file)
+    targetIndex = 0
+    clip = makeEmptySound(15340)
+    for ii in range(0, 15340):
+        x = getSampleValueAt(sound, ii)
+        setSampleValueAt(clip, targetIndex, x)
+        targetIndex = targetIndex + 1
+    max = maxVolume(clip)
+    play(clip)
+
+
+def maxSample(sound):
+    max_val = 0
+    for sample in getSamples(sound):
+        max_val = max(max_val, getSampleValue(sample))
+    return max_val
+
+
+def maxVolume(sound):
+    largest = maxSample(sound)
+    maxPossibleSampleValue = 32767.0
+    factor = float(maxPossibleSampleValue) / largest
+    for sample in getSamples(sound):
+        value = getSampleValue(sample)
+        setSampleValue(sample, value * factor)
+    return sample
+
+
+def resize(pic):
+    width = getWidth(pic)
+    height = getHeight(pic)
+    target = makeEmptyPicture(1000, 1000)
+    percentWidth = width / 1000.0
+    percentHeight = height / 1000.0
+    for x in range(0, 999):
+        for y in range(0, 999):
+            color = getColor(getPixel(pic, int(x * percentWidth), int(y * percentHeight)))
+            setColor(getPixel(target, x, y), color)
+    return target
+
+
+def win_image(image_file):
+    pic = makePicture(image_file)
+    pic = resize(pic)
+    pixels = getPixels(pic)
+    for p in pixels:
+        newColor = makeColor(getRed(p) + 50, getGreen(p) + 50, getBlue(p) + 50)
+        setColor(p, newColor)
+
+    phrase = "You win!"
+    import java.awt.Font as Font
+    myFont = makeStyle("Comic Sans", Font.BOLD, 80)
+    addTextWithStyle(pic, 350, 500, phrase, myFont, black)
+    repaint(pic)
+
+
+def lose_image(image_file):
+    pic = makePicture(image_file)
+    pic = resize(pic)
+    pixels = getPixels(pic)
+    for p in pixels:
+        avg_color = avg_color = (getRed(p) * .299 + getGreen(p) * .587 + getBlue(p) * .114)
+        newColor = makeColor(avg_color, avg_color, avg_color)
+        setColor(p, newColor)
+
+    phrase = "You lose."
+    import java.awt.Font as Font
+    myFont = makeStyle("Comic Sans", Font.BOLD, 80)
+    addTextWithStyle(pic, 350, 500, phrase, myFont, red)
+    repaint(pic)
+
+
+def win():
+    setMediaPath()
+    win_image = getMediaPath() + 'win.jpg'
+    lose_image = getMediaPath() + 'lose.jpg'
+    win_sound = getMediaPath() + 'win.wav'
+    lose_sound = getMediaPath() + 'lose.wav'
 
 
 class Player:
@@ -85,8 +93,10 @@ class Player:
         self.resources = resources
 
     def print_stats(self):
-        print self.moves
-        print self.resources
+        print "*********************************************************"
+        print "[You have " + str(self.moves) + " moves left]"
+        print "[You have the following resources " + str(self.resources)
+        print "*********************************************************"
 
 
 class Tile(object):
@@ -129,6 +139,7 @@ def welcome():
     print "We found shelter on a new technological advancement, free floating sky islands that kept us away from " \
           "the pink menace."
     print "But a troubling message came in during my shift at the Onlook tower."
+    print "Careful, any wasted movement will lose you some time!"
 
 
 class TowerStart(Tile):
@@ -145,25 +156,45 @@ class TowerStart(Tile):
               "and people of the message."
         print "...Unless this is just a cruel joke from the other island"
         print "Let's head over East towards the Barracks, Citadel and Suburbs to notify the others of the message"
+        print "ONCE YOU HAVE ALL THE RESOURCES, RETURN HOME TO THE TOWER!!! HURRY!"
 
-    def win(self, player):
         if 'Radar' in self.player.resources:
-            print "you have it!"
-            return
+            print "Bill install the radar to the tower's system and you see 4 Unknown Flying Objects coming from the West"
+            print "The UFO's should be enough proof for the Comander.\n"
+        elif 'Radar' in self.player.resources and 'Proof' in self.player.resources:
+            print "Let's alert everyone that pigs are flying!\n"
         else:
-            print "You are missing resources."
+            print "Trying to get in contact with the other island is fruitless"
+            print "Your messages aren't responded.\n"
+
+        print "you can only go 'e'-East towards the Barracks\n\n"
+
+    def take_resource(self):
+        if 'Proof' not in self.player.resources:
+            print "When you showed the picture of the UFO's, the Comander shouted at everybody to get in the artillery to protect the island"
+            print "We should be closer in defending the island.\n"
+            self.player.resources.append('Proof')
+        else:
+            print "The Comander is waiting impatiently for any means to prove the message.\n"
+
+
+    def win(self):
+        if 'Radar' in self.player.resources and 'Wyvern' in self.player.resources and 'People Safe' in self.player.resources:
+            print "**********-You Win!-**********"
+            print "The artillery shoots at the planes before the pigs get in enough range."
+            print "The Mayor's Wyvern takes flight and clips off a couple pigs from the sky."
+            print "The remaining pigs fire at the suburbs but their shots land on the barren districts."
+            print "Nobody was damaged and the remaining flyings pigs are taken care of.\n\n"
+            print "However you thought you'd never see the day pigs fly... It is time we take our planet away from them!"
+            print "You have won today!"
+            return 'win'
+        else:
+            print "\nYou are missing resources!!!!!!!! Hurry!!!!"
 
 
 class Barracks(Tile):
     def __init__(self, x, y, player, *args):
         super(Barracks, self).__init__(x, y, player, *args)
-
-    def take_radar(self):
-        if 'Radar' not in self.player.resources:
-            print "You have received the radar"
-            self.player.resources.append('Radar')
-        else:
-            print "Already been here and taken the radar"
 
     def print_room_text(self):
         print "**********-Barracks-**********"
@@ -172,6 +203,10 @@ class Barracks(Tile):
         print "He mentions he needs proof before he allows you to get the defenses loaded. You should look for " \
               "a means to locate were they are coming from.\n"
 
+        print "-Go 'n' to the Artillery"
+        print "-Go 'e' to the Citadel"
+        print "-Go 'w' to the Onlook tower"
+        print "-Go 's' to the Lab\n\n"
 
 class Artillery(Tile):
     def __init__(self, x, y, *args):
@@ -182,6 +217,19 @@ class Artillery(Tile):
         print "The mighty Cannons that protect your island stand here. But nobody is manning them. You can't " \
               "defend the island alone.\n"
 
+        if 'Proof' in self.player.resources:
+            print "Your brothers in arms stand in wait to attack the flying pigs once they get in range!\n"
+
+        print "-Go 's' to the Barracks"
+        print "-Go 'e' to the Suburbs\n\n"
+
+    def take_resource(self):
+        if 'Artillery Manned' not in self.player.resources:
+            print "You have received the Artillery Manned"
+            self.player.resources.append('Artillery Manned')
+        else:
+            print "Already been here and taken the Artillery Manned"
+            print "Let's hurry before it is too late!\n"
 
 class Laboratory(Tile):
     def __init__(self, x, y, *args):
@@ -192,6 +240,18 @@ class Laboratory(Tile):
         print "The Laboratory is where all the new technology is created. If we bring a scientist with us we could " \
               "look for an upgrade for our Tower.\n"
 
+        if 'Scientist' in self.player.resources:
+            print "Bill works at the Laboratory and unlocks the gate."
+            print "He grabs the new Radar prototype he was working on for your Tower, this should give you enough proof if pigs are really coming!\n"
+        print "-Go 'n' to the Barracks\n\n"
+
+    def take_resource(self):
+        if 'Radar' not in self.player.resources:
+            print "You have received the radar"
+            self.player.resources.append('Radar')
+        else:
+            print "Already been here and taken the radar"
+            print "Let's hurry before it is too late!\n"
 
 class Citadel(Tile):
     def __init__(self, x, y, *args):
@@ -204,6 +264,32 @@ class Citadel(Tile):
         print "Unfortunately he is very busy documenting the taxes that the population owe him and tells you it's up to you to collect his pet frogs at the lake with a bug net."
         print "...you begin to question the Mayor's leadership.\n"
 
+        if 'Frogs' in self.player.resources:
+            print "The Mayor thanks you and throws the frogs into an open hole in the corner of the room."
+            print "From the window you see a wyvern emerge from the side of the Citadel and see it go towards the Onlook Tower."
+            print "Maybe the Mayor is on this position for a reason...\n"
+        elif 'Wyvern' in self.player.resources:
+            print "The Mayor has released a Wyvern to defend the island. But other's should be warned\n"
+        else:
+            print "The Mayor just shooshs you away telling you to collect his pet frogs from the lake.\n"
+
+        print "-Go 'n' to the Suburbs"
+        print "-Go 'e' to the Sanctuary"
+        print "-Go 'w' to the Barracks\n\n"
+
+    def take_resource(self):
+        if 'Frog Net' not in self.player.resources:
+            print "You have received the Frog Net"
+            self.player.resources.append('Frog Net')
+        else:
+            print "Already been here and taken the radar"
+
+    def take_wyvern(self):
+        if 'Wyvern' not in self.player.resources:
+            print "You have received the Wyvern"
+            self.player.resources.append('Wyvern')
+        else:
+            print "Already been here and taken the Wyvern"
 
 class Suburbs(Tile):
     def __init__(self, x, y, *args):
@@ -215,6 +301,22 @@ class Suburbs(Tile):
         print "Fortunately you recognize Bill, your scientist buddy who decides to join your party after hearing your plea."
         print "Bill can rally his friends to make everyone flee, now you need to find a place for everyone to shelter in.\n"
 
+        if 'Scientist' in self.player.resources:
+            print "We should find somewhere where people can find shelter!\n"
+        if 'People Safe' in self.player.resources:
+            print "We need to warn the others of the danger coming in, we don't have much time!\n"
+
+        print "-Go 'e' to the Suburbs"
+        print "-Go 'w' to the Sanctuary\n\n"
+
+    def take_resource(self):
+        if 'Scientist' not in self.player.resources:
+            print "You have received the Scientist"
+            self.player.resources.append('Scientist')
+        else:
+            print "Already been here and taken the Scientist"
+
+
 
 class Lake(Tile):
     def __init__(self, x, y, *args):
@@ -223,6 +325,21 @@ class Lake(Tile):
     def print_room_text(self):
         print "**********-Lake-**********"
         print "A lake in the corner of your island and the source of fresh water. Not much for us to do here.\n"
+
+        if 'Frog Net' in self.player.resources:
+            print "You spend some time but capture all the frogs, you wonder if this is a good use of your time.\n"
+        if 'Frogs' in self.player.resources:
+            print "Let's hurry and alert everyone before it is too late!\n"
+
+        print "-Go 'w' to the Suburbs"
+        print "-Go 's' to the Sanctuary\n\n"
+
+    def take_resource(self):
+        if 'Frogs' not in self.player.resources:
+            print "You have received the Frogs"
+            self.player.resources.append('Frogs')
+        else:
+            print "Already been here and taken the Frogs"
 
 
 class Sanctuary(Tile):
@@ -236,6 +353,15 @@ class Sanctuary(Tile):
         print "She, reluctantly, hears your story and tells you she will shelter everyone down at the catacombs."
         print "Now we just need to alert the people to come take shelter.\n"
 
+        print "-Go 'w' to the Citadel\n\n"
+
+    def take_resource(self):
+        if 'People Safe' not in self.player.resources:
+            print "You have received the People Safe"
+            self.player.resources.append('People Safe')
+        else:
+            print "Already been here and taken the People Safe"
+
 
 def room_execute(room, player):
     location = room
@@ -246,11 +372,11 @@ def room_execute(room, player):
 
 
 def main():
-    # setMediaPath()
-    # win_image_file = getMediaPath() + 'win.jpg'
-    # lose_image_file = getMediaPath() + 'lose.jpg'
-    # win_sound_file = getMediaPath() + 'win.wav'
-    # lose_sound_file = getMediaPath() + 'lose.wav'
+    setMediaPath()
+    win_image_file = getMediaPath() + 'win.jpg'
+    lose_image_file = getMediaPath() + 'lose.jpg'
+    win_sound_file = getMediaPath() + 'win.wav'
+    lose_sound_file = getMediaPath() + 'lose.wav'
 
     init_move = 20
     resources = []
@@ -260,38 +386,52 @@ def main():
     barracks = Barracks(1, 1, player_1, 'n', 'e', 'w', 's')
     artillery = Artillery(1, 2, player_1, 's', 'e')
     laboratory = Laboratory(1, 0, player_1, 'n')
-    citadel = Laboratory(1, 0, player_1, 'n', 'e', 'w')
-    suburbs = Suburbs(1, 0, player_1, 'n', 'e', 'w')
-    lake = Lake(3, 2, player_1, 'n', 'e', 'w')
-    sanctuary = Sanctuary(3, 1, player_1, 'n', 'e', 'w')
+    citadel = Citadel(2, 1, player_1, 'n', 'e', 'w')
+    suburbs = Suburbs(2, 2, player_1, 'e', 'w', 's')
+    lake = Lake(3, 2, player_1, 's', 'w')
+    sanctuary = Sanctuary(3, 1, player_1, 'w', 'n')
 
     welcome()
 
     location = onlook_tower_start
     location.print_room_text()
 
-    while direction != 'quit':
+    while direction != 'win' and direction != 'quit' and player_1.moves > 0:
         player_1.print_stats()
         direction = raw_input("What direction do you want to go [n,w,s,e]? ").lower()
         location.move(direction)
         if location.print_room() == (0, 1):
             location = room_execute(onlook_tower_start, player_1)
+            if 'Radar' in player_1.resources:
+                location.take_resource()
+            direction = location.win()
         elif location.print_room() == (1, 1):
             location = room_execute(barracks, player_1)
-            location.take_radar()
         elif location.print_room() == (1, 2):
             location = room_execute(artillery, player_1)
+            if 'Proof' in player_1.resources:
+                location.take_resource()
         elif location.print_room() == (1, 0):
             location = room_execute(laboratory, player_1)
+            if 'Scientist' in player_1.resources:
+                location.take_resource()
         elif location.print_room() == (2, 1):
             location = room_execute(citadel, player_1)
+            location.take_resource()
+            if 'Frogs' in player_1.resources:
+                location.take_wyvern()
         elif location.print_room() == (2, 2):
             location = room_execute(suburbs, player_1)
+            location.take_resource()
         elif location.print_room() == (3, 2):
             location = room_execute(lake, player_1)
+            if 'Frog Net' in player_1.resources:
+                location.take_resource()
         elif location.print_room() == (3, 1):
             location = room_execute(sanctuary, player_1)
+            location.take_resource()
 
-
-if __name__ == '__main__':
-    main()
+    if direction == 'lose':
+        print "**********-You Lose-**********"
+        print "It's too late... You thought you'd never see the day but, the day pigs fly is here and nobody believed you."
+        print "You brace yourself as the pink menace opens fire to the island..."
